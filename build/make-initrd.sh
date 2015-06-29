5c168053841b807b33037c309637130acb750af2
Gao feng <feng@hyper.sh>
2015-06-29 11:03:04 +0800

    make cbfs rom through Makefile
    `make cbfs` will generate cbfs.rom under build directory.
    
    Signed-off-by: Gao feng <feng@hyper.sh>

--- build/make-initrd.sh
+++ build/make-initrd.sh
@@ -19,3 +19,22 @@ do
 	done
 done
 cd ./root && find . | cpio -H newc -o | gzip -9 > ../hyper-initrd.img
+
+
+if [ "$1" != "cbfs" ]; then
+	exit 0
+fi
+
+cd ../
+
+echo "build cbfs"
+rm -rf .cbfs
+rm -rf cbfs.rom
+
+mkdir .cbfs
+dd if=/dev/zero of=.cbfs/boot.bin bs=4096 count=1
+cbfstool .cbfs/cbfs.rom create -s 4096k -B .cbfs/boot.bin -m x86  0x1000
+cbfstool .cbfs/cbfs.rom add -f kernel -n vmlinuz -t raw
+cbfstool .cbfs/cbfs.rom add -f hyper-initrd.img -n initrd -t raw
+cp .cbfs/cbfs.rom ./
+rm -rf .cbfs
