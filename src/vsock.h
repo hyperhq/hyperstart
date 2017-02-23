b1d71619ce98a24d7f80f36da3bedfe94c0acf65
Peng Tao <bergwolf@gmail.com>
2017-02-23 10:26:34 +0800

    optionally build with vsock support
    If we cannot find /usr/include/linux/vm_sockets.h locally, do not build
    with vsock support.
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- src/vsock.h
+++ src/vsock.h
@@ -2,10 +2,28 @@
 #define _VSOCK_H_
 
 #include "event.h"
+#include "../config.h"
 
+#ifdef HAVE_VSOCK
 int probe_vsock_device(void);
 int hyper_create_vsock_listener(unsigned short port);
 int hyper_vsock_accept(struct hyper_event *he, int efd,
 		       struct hyper_event *ne, struct hyper_event_ops *ops);
+#else /*HAVE_VSOCK*/
+static int probe_vsock_device(void)
+{
+	return -1;
+}
+static int hyper_create_vsock_listener(unsigned short port)
+{
+	return -1;
+}
+static int hyper_vsock_accept(struct hyper_event *he, int efd,
+		       struct hyper_event *ne, struct hyper_event_ops *ops)
+{
+	return -1;
+}
+
+#endif /*HAVE_VSOCK*/
 
 #endif
